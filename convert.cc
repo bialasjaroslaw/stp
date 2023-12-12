dd82ccf2e392197c7d823f7d493a6d5e7ebe9c87
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2023-12-12 21:22:37 +0100

    Add 64 bit data types test

--- convert.cc
+++ convert.cc
@@ -10,7 +10,7 @@ TEST(Convert, VectorOfStringsToVectorOfInts)
 {
     using namespace std::literals;
     std::vector<std::string> data{"12"s, "-3"s, "2147483647"s, "-2147483648"s};
-    auto result = Text::convert<int>(data);
+    auto result = Text::convert<int32_t>(data);
     ASSERT_THAT(result, ElementsAre(12, -3, 2147483647, -2147483648));
 }
 
@@ -18,7 +18,7 @@ TEST(Convert, VectorOfStringViewsToVectorOfInts)
 {
     using namespace std::literals;
     std::vector<std::string_view> data{"12"sv, "-3"sv, "2147483647"sv, "-2147483648"sv};
-    auto result = Text::convert<int>(data);
+    auto result = Text::convert<int32_t>(data);
     ASSERT_THAT(result, ElementsAre(12, -3, 2147483647, -2147483648));
 }
 
@@ -27,8 +27,24 @@ TEST(Convert, VectorOfStringsOutOfRange)
     using namespace std::literals;
     std::vector<std::string_view> data{"2147483648"s};
     EXPECT_THAT([&data](){ 
-    	Text::convert<int>(data); },
+        Text::convert<int32_t>(data); },
     Throws<std::runtime_error>(Property(&std::runtime_error::what,
          HasSubstr("Invalid value passed to convert"))));
 
+}
+
+TEST(Convert, VectorOfStringsToVectorOfLongs)
+{
+    using namespace std::literals;
+    std::vector<std::string> data{"12"s, "-3"s, "9223372036854775807"s, "-9223372036854775808"s};
+    auto result = Text::convert<int64_t>(data);
+    ASSERT_THAT(result, ElementsAre(12, -3, 9223372036854775807, -9223372036854775807-1));
+}
+
+TEST(Convert, VectorOfStringViewsToVectorOfLongs)
+{
+    using namespace std::literals;
+    std::vector<std::string_view> data{"12"sv, "-3"sv, "9223372036854775807"sv, "-9223372036854775808"sv};
+    auto result = Text::convert<int64_t>(data);
+    ASSERT_THAT(result, ElementsAre(12, -3, 9223372036854775807, -9223372036854775807-1));
 }
